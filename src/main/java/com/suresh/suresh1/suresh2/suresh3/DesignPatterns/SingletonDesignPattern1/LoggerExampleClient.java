0bbf40a9912cf8f8d597dfe3da5163c8c3d82b40
suresh chandra <sureshchmca786@gmail.com>
2024-07-19 15:44:06 +0530

    Added Singleton Design Pattern

---
+++ src/main/java/com/suresh/suresh1/suresh2/suresh3/DesignPatterns/SingletonDesignPattern1/LoggerExampleClient.java
@@ -0,0 +1,9 @@
+package com.suresh.suresh1.suresh2.suresh3.DesignPatterns.SingletonDesignPattern1;
+
+public class LoggerExampleClient {
+    public static void main(String[] args) {
+        Logger logger = Logger.getInstance();
+        logger.log("This is a log message."); // Output: Log: This is a log message.
+    }
+
+}
