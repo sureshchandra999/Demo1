1023dea72f38342d3200b78c1a4164d46c16c5ad
suresh chandra <sureshchmca786@gmail.com>
2024-07-27 17:23:07 +0530

    Added Singleton pattern example

---
+++ src/main/java/com/suresh/April/Suryanshu.java
@@ -0,0 +1,9 @@
+package com.suresh.April;
+
+public class Suryanshu {
+    public static void main(String[] args) {
+        for (int i=1; i<= 100; i++) {
+            System.out.println("Suryanshu Sakhar Ghana");
+        }
+    }
+}
---
+++ src/main/java/com/suresh/suresh1/suresh2/suresh3/DesignPatterns/SingletonDesignPattern3/SingletonPatternDemo.java
@@ -0,0 +1,10 @@
+package com.suresh.suresh1.suresh2.suresh3.DesignPatterns.SingletonDesignPattern3;
+
+public class SingletonPatternDemo {
+    public static void main(String[] args) {
+        Singleton singleton = Singleton.getInstance();
+        singleton.showMessage();
+
+
+    }
+}
