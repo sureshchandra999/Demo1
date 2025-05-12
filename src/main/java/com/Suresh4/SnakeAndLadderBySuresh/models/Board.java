895fc33052e010ff2baf5a71427c4f62dbe0baf7
suresh chandra <sureshchmca786@gmail.com>
2025-05-12 11:05:27 +0530

    Snake and Ladder Game model class committed

--- src/main/java/com/Suresh4/SnakeAndLadderBySuresh/models/Board.java
+++ src/main/java/com/Suresh4/SnakeAndLadderBySuresh/models/Board.java
@@ -1,10 +1,27 @@
 package com.Suresh4.SnakeAndLadderBySuresh.models;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Board {
     private int dimension;
-    private Map<Integer, BoardEntity> boardEntitiesMap;
+    private int cellCount;
+    private Map<Integer, BoardEntity> boardEntitiesMap; // this map will contain snakes and ladders
+
+    // Constructor
+    public Board(int dimension) {
+        this.dimension = dimension;
+        this.cellCount = dimension * dimension;
+
+        // Initialize n snakes and n ladders
+        boardEntitiesMap = new HashMap<>();
+        //Add n snake   start index is > end index.
+
+
+
+
+    }
+
 
     public int getDimension() {
         return dimension;
@@ -22,3 +39,19 @@ public class Board {
         this.boardEntitiesMap = boardEntitiesMap;
     }
 }
+/*
+roll a dice
+ min=1, max=6
+ random number between 1 and 6
+
+ (0,1) * (6 - 1 + 1) + 1 =>=> floor(x * 6 + 1) where x lies in (0,1)
+
+ x=0.1, => 1.6 -> floor(1.6) => 1
+x=0.7, => 5.2 -> floor(5.2) => 5
+x=0.9, => 6.4 -> floor(6.4) => 6
+x=0.6, => 4.6 -> floor(4.6) => 4
+
+
+
+
+ */
